91c71cf64876d96e2ff64aa93a80d1dcd3bd4675
Reuben <rleibgenkin@gmail.com>
2021-11-30 23:13:44 -0500

    added section component

---
+++ src/components/Section/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+function Project(props) {
+        return (
+
+                <section id="Project" className="container-fluid">
+                        <div className="row header-wrapper offset-1 projects-header-wrapper">
+                                <h2 className="section-header projects-header">Projects</h2>
+                        </div>
+
+                </section>
+        );
+}
+
+export default Project;

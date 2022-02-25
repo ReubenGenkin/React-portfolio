22e66406f7fc7afe6c71b0f9917c9b84d16ccca8
Reuben <rleibgenkin@gmail.com>
2022-02-25 12:38:55 -0500

    updates to nav

--- src/components/About.js
+++ src/components/About.js
@@ -13,16 +13,17 @@ function About() {
         <div className="about-content grid-item-content">
           <p className="about-margin">
             Junior full stack developer proficient in core development
-            technologies, such as JavaScript, HTML5, CSS, REACT.js and other
-            technologies. I utilize my background in sales and the food service
-            industry to effectively communicate and adapt within the AGILE
-            workflow. In my work I bring a focus to DRY and concise code with
-            other developers, as well as interactive, dynamic, and accessible
-            design for users. I earned my full stack web development certificate
-            from Rutgers Coding Bootcamp. When programming, I efficiently and
-            effectively find solutions through collaborating with team members,
-            as well as tools like GitHub.My goal with my work is to advance
-            accessibility, and find creative ways to enhance the user.
+            technologies, such as JavaScript, HTML5, CSS, REACT.js and others. I
+            utilize my background in sales and the food service industry to
+            effectively communicate and adapt within the AGILE workflow. In my
+            work I bring a focus to DRY, concise, and collaborative code working
+            with other developers, as well as interactive, dynamic, and
+            accessible design for users. I earned my full stack web development
+            certificate from Rutgers Coding Bootcamp. When programming, I
+            efficiently and effectively find solutions through collaborating
+            with team members, as well as tools like GitHub. My goal with my
+            work is to maintain high levels of accessibility, and find creative ways to enhance
+            the user experience.
           </p>
         </div>
       }
